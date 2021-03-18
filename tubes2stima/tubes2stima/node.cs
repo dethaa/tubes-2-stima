47e5bec0afaf65436acdc75dd4e8b60539cc1b55
nabelanita <utami.nabelanita@gmail.com>
2021-03-18 22:59:31 +0700

    tambah method baru node

--- tubes2stima/tubes2stima/node.cs
+++ tubes2stima/tubes2stima/node.cs
@@ -2,38 +2,70 @@
 using System.Collections.Generic;
 
 public class Node {
+    // attributes
     private string name;
     private List<string> adj;
     private int numAdj;
     private bool visited;
 
+
+    // constructor
     public Node(string name)
     {
         this.name = name;
         adj = new List<string>();
-        this.numAdj = 0;
-        this.visited = false;
+        numAdj = 0;
+        visited = false;
         Console.WriteLine("ctor ok");
     }
 
+    // getter
+
+    // mengambil nama node
     public string getName() { return name; }
-    public string getAdjOnIdx(int idx) { return this.adj[idx]; }
+    public List<string> getAllAdj() { return adj; }
+    public string getAdjOnIdx(int idx) { return adj[idx]; }
     public int getNumAdj() { return numAdj; }
     public bool isVisited() { return visited; }
+
+    // methods
+
+    // menambahkan tetangga baru
+    public void addNewAdj(string name)
+    {
+        adj.Add(name);
+        numAdj++;
+    }
+
+    // memeriksa apakah nama node sama dengan ...
+    public bool isNodeName(string name) { return this.name == name; }
+
+    // mengubah status node menjadi telah dikunjungi
+    public void hasVisited() { visited = true; }
+
+    // mengubah status node menjadi belum dikunjungi
+    public void notVisited() { visited = false; }
+
+    // output methods
+
+    // menampilkan semua tetangga ke layar
     public void printAdj()
     {
         int i;
-        for(i = 0; i < this.numAdj; i++)
+        for(i = 0; i < numAdj; i++)
         {
-            Console.Write(this.getAdjOnIdx(i)+ " ");
+            Console.Write(getAdjOnIdx(i)+ " ");
         }
         Console.WriteLine("\n");
     }
+
+    // menampilkan informasi node
     public void showDetails()
     {
-        Console.WriteLine("Nama node: " + this.name);
-        Console.WriteLine("Jumlah tetangga: " + this.numAdj);
+        Console.WriteLine("Nama node: " + name);
+        Console.WriteLine("Jumlah tetangga: " + numAdj);
         Console.Write("Daftar tetangga: ");
-        this.printAdj();
+        printAdj();
+        Console.WriteLine("Dikungjungi: " + visited)
     }
 }
