7abab3b5d18f8b3ee0645c9d29498f54ac93ead9
dethaa <sharonmarbun12@gmail.com>
2021-03-19 21:41:37 +0700

    masih coba input file

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 namespace Application
 {
     class inputFile {
-        /*
-        static void Main(string[] args)
-        {
+        
+        static void Main(string[] args) {
+            int i;
             //Console.Write("Masukkan nama file: ");
             //string fileName = Console.ReadLine();
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\HP\Documents\C#\test1.txt");
             //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\HP\Documents\C#\" + fileName);
             //System.Console.WriteLine("Isi dari " + fileName + ":");
             foreach (string line in lines){
-                Console.WriteLine(line);
+                //Console.WriteLine(line);
             }
             int count = Int32.Parse(lines[0]);
             //System.Console.WriteLine(lines.Length);
@@ -21,67 +21,41 @@ namespace Application
             //foreach (string word in words){
                 //Console.WriteLine(word);
 
-            int i;
-            for (i=1;i<count;i++){
+            List<List<string>> listAll = new List<List<string>>();
+            List<string> listTemp = new List<string>();
+            for(i=1;i<count;i++){
+                int index1,index2;
                 string[] words = lines[i].Split(' ');
-                foreach (string word in words){
-                    Console.WriteLine(word);
+                index1 = findString(words[0],listAll);
+                if (index1 == -1){
+                    listTemp.Clear();
+                    listTemp.AddRange(words);
+                } else {
+                    listAll[index1].Add(words[1]);
+                }
+
+                index2 = findString(words[1],listAll);
+                if (index2 == -1){
+                    listTemp.Clear();
+                    listTemp.Add(words[1]);
+                    listTemp.Add(words[0]);
+                } else {
+                    listAll[index2].Add(words[0]);
                 }
             }
-        } */
+            listAll.ForEach(Console.WriteLine);
+        } 
 
-        public void input(string fileName){
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\HP\Documents\C#\" + fileName);
-            System.Console.WriteLine("Isi dari " + fileName + ":");
-            int count = Int32.Parse(lines[0]);
-
-            List<List<string>> list1 = new List<List<string>>();
-            
-            int i;
-            for (i=1;i<count;i++){
-                List<string> list2 = new List<string>();
-                string[] words = lines[i].Split(' ');
-                foreach (string word in words){
-                    list2.Add(word); 
+        public static int findString(string str, List<List<string>> listAll){
+            int k = 0;
+            while (k < listAll.Count){
+                if (str == listAll[k][0]){
+                    return k;
                 }
-                list1.Add(list2);
-                //A,B
+                k++;
             }
+            return -1;
         }
     } 
-    public class Node {
-        private string name;
-        private List<string> adj;
-        private int numAdj;
-        private bool visited;
 
-        public Node(string name){
-        this.name = name;
-        adj = new List<string>();
-        this.numAdj = 0;
-        this.visited = false;
-        Console.WriteLine("ctor ok");
-        }
-        public string getName() { return name; }
-        public string getAdjOnIdx(int idx) { return this.adj[idx]; }
-        public int getNumAdj() { return numAdj; }
-        public bool isVisited() { return visited; }
-        public void printAdj()
-        {
-            int i;
-            for(i = 0; i < this.numAdj; i++)
-            {
-                Console.Write(this.getAdjOnIdx(i)+ " ");
-            }
-            Console.WriteLine("\n");
-        }
-        public void showDetails()
-        {
-            Console.WriteLine("Nama node: " + this.name);
-            Console.WriteLine("Jumlah tetangga: " + this.numAdj);
-            Console.Write("Daftar tetangga: ");
-            this.printAdj();
-        }
-    }
- 
 }
