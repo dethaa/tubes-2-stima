4c7a433da5f8003ee33d5db9b1adb837281a77ee
epata <epatatuah180901@gmail.com>
2021-03-20 20:36:01 +0700

    update fronent

--- frontend/randomSTIMA/Random.cs
+++ frontend/randomSTIMA/Random.cs
@@ -51,6 +51,16 @@ namespace randomSTIMA
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
 
